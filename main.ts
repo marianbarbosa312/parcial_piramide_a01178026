c9a2370935d754847765105af91fc6b81d0d5188
marianbarbosa312 <70342506+marianbarbosa312@users.noreply.github.com>
2020-09-17 13:30:24 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Volumen de la piramide =")
+    basic.showNumber(Pi * Radio ** 2 * Altura)
+})
+let Altura = 0
+let Radio = 0
+let Pi = 0
+basic.showString("Marian Barbosa Arellano   A01178026")
+Pi = 3.14
+Radio = 5
+Altura = 15
